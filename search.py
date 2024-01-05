e1584bc8987de09877eebf38cbcc67b019b86df3
0xberkay <o200706008@gmail.com>
2024-01-05 23:19:05 +0300

    Add color highlighting

--- search.py
+++ search.py
@@ -1,5 +1,6 @@
 import requests
 import sys
+import renkler
 
 def get_all_user_followers(username):
     followers_url = f"https://api.github.com/users/{username}/followers"
@@ -14,24 +15,30 @@ def get_all_user_followers(username):
 
             followers_url = response.links.get('next', {}).get('url')
         else:
-            print(f"Failed to fetch followers for {username}. Status code: {response.status_code}")
+            print(f"{renkler.kirmizi}Failed to fetch followers for {username}. Status code: {response.status_code}{renkler.sifirla}")
             return None
 
     return followers
+
+#Random color list
+color_list = [renkler.kirmizi,renkler.turuncu,renkler.sari,renkler.lacivert,renkler.pembe,renkler.mor,renkler.mavi,renkler.turkuaz]
 
 def list_followers(username):
     followers = get_all_user_followers(username)
 
     if followers:
-        print(f"Followers of {username}:")
+        print(f"{renkler.yesil}Followers of {username}:")
         for follower in followers:
+            print(renkler.rastgeleRenkler(*color_list))
             print(f"- {follower['login']}")
 
-        print(f"\nTotal Followers: {len(followers)}")
+        print(f"\n{renkler.yesil}Total followers: {len(followers)}")
+
+    print(renkler.sifirla)
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: python script.py <github_username>")
+        print(f"{renkler.kirmizi}Usage: {sys.argv[0]} <username>{renkler.sifirla}")
         sys.exit(1)
 
     username = sys.argv[1]
